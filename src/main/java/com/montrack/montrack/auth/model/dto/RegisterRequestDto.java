d1fb7f0e70b22e8facc1ed31f2bea838705bb02f
Gerry Sheva <gerry.sheva@proton.me>
2024-06-14 18:54:41 +0700

    fix:login&registration added validation in dtos

--- src/main/java/com/montrack/montrack/auth/model/dto/RegisterRequestDto.java
+++ src/main/java/com/montrack/montrack/auth/model/dto/RegisterRequestDto.java
@@ -1,9 +1,13 @@
 package com.montrack.montrack.auth.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RegisterRequestDto {
+    @Email
     private String email;
+    @NotBlank
     private String password;
 }
